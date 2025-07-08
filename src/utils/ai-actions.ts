c468fec9c551cfb8602edc764ca07211f62457ee
Tyler Pina <tyler.pina@contentful.com>
2025-07-08 13:58:46 -0600

    chore: ai-actions util file created

---
+++ src/utils/ai-actions.ts
@@ -0,0 +1,9 @@
+export declare enum VariableType {
+  RESOURCE_LINK = 'ResourceLink',
+  TEXT = 'Text',
+  STANDARD_INPUT = 'StandardInput',
+  LOCALE = 'Locale',
+  MEDIA_REFERENCE = 'MediaReference',
+  REFERENCE = 'Reference',
+  SMART_CONTEXT = 'SmartContext',
+}
