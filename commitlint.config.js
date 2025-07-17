f3d13022b457891b7b2d85c2210e8c03f2ef9bae
Tyler Pina <tyler.pina@contentful.com>
2025-07-17 14:00:43 -0600

    chore: commitlint commit validation implemented

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [0, 'always', ['lower-case', 'pascal-case', 'camel-case']],
+    'subject-case': [0, 'always', ['lower-case', 'sentence-case']],
+  },
+};
