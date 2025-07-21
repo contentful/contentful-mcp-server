61b11a3c462505482254aedcfc8368d762103d19
Tyler Pina <tyler.pina@contentful.com>
2025-07-21 15:28:47 -0600

    chore: commitlint config updated

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,1 @@
-//validate whether commit messages follow Angular JS Commit Message Conventions
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-case': [0, 'always', ['lower-case', 'pascal-case', 'camel-case']],
-    'subject-case': [0, 'always', ['lower-case', 'sentence-case']],
-  },
-};
+export default { extends: ['@commitlint/config-conventional'] };
